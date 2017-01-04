c7fccccb403a82a051a33ae9ab820affd54b70eb
myYaYa0123 <my_ya1223@163.com>
2017-01-04 00:50:19 -0600

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,102 @@
+/**
+ * Created by admin on 2016/12/9.
+ */
+var a = {
+    "success": true,
+    "errorCode": "S00001",
+    "errorMsg": null,
+    "data": {
+        "pageInfo": {
+            "totalItem": 15,
+            "pageSize": 20,
+            "currentPage": 1,
+            "totalSum": {
+                "amount": 0.00,
+                "currency": "CNY",
+                "cent": 0,
+                "centFactor": 100
+            },
+            "startRow": 0,
+            "endRow": 0,
+            "needQeryTotal": false,
+            "needDelete": false,
+            "needQueryAll": false,
+            "allowedQuery": true,
+            "pageFristItem": 1,
+            "pageLastItem": 15,
+            "totalPage": 1
+        },
+        "data": [{
+            "created": 1468314385000,
+            "creatTime": "2016-07-12 17:06:25",
+            "creater": null,
+            "orderNo": "1607120906250594",
+            "ordDesc": null,
+            "remark": null,
+            "price": 0.01,
+            "payed": 0.01,
+            "proxAmount": 0.00,
+            "fee": null,
+            "settlement": null,
+            "status": "SETTLEED",
+            "orderStatus": "1",
+            "goodName": null,
+            "flows": [{
+                "operNo": null,
+                "orderNo": null,
+                "createTime": null,
+                "updateTime": null,
+                "payTime": null,
+                "bankName": null,
+                "payType": null,
+                "innoFlowNo": "1607120906271024",
+                "remark": null,
+                "amount": 0.01,
+                "money": 0.01,
+                "proxAmount": 0,
+                "status": "PAY_SUCCESS",
+                "fee": null,
+                "refundOperNo": null,
+                "refundTime": null,
+                "settleStatus": null,
+                "cardType": null
+            }]
+        }, {
+            "created": 1468314324000,
+            "creatTime": "2016-07-12 17:05:24",
+            "creater": null,
+            "orderNo": "1607120905240593",
+            "ordDesc": null,
+            "remark": null,
+            "price": 0.01,
+            "payed": 0.01,
+            "proxAmount": 0.00,
+            "fee": null,
+            "settlement": null,
+            "status": "SETTLEED",
+            "orderStatus": "1",
+            "goodName": null,
+            "flows": [{
+                "operNo": null,
+                "orderNo": null,
+                "createTime": null,
+                "updateTime": null,
+                "payTime": null,
+                "bankName": null,
+                "payType": null,
+                "innoFlowNo": "1607120905281023",
+                "remark": null,
+                "amount": 0.01,
+                "money": 0.01,
+                "proxAmount": 0,
+                "status": "PAY_SUCCESS",
+                "fee": null,
+                "refundOperNo": null, 
+
+                "refundTime": null,
+                "settleStatus": null,
+                "cardType": null
+            }]
+        }]
+    }, "redirectUrl": null, "headerNavLink": null, "sideNavLink": null, "tabNavLink": null
+}
